602afebc8c62a03d7a9f447681b6497ec7287eb9
Bhavesh Jain <70514193+bhaveshjain1612@users.noreply.github.com>
2023-08-17 18:58:48 +0530

    Update In Depth Stock Analysis.py

--- pages/In Depth Stock Analysis.py
+++ pages/In Depth Stock Analysis.py
@@ -281,8 +281,8 @@ def generate_news(name):
             col1.write(news.source[i])
             col2.write(news.date[i])
             url  = "https://"+news.link[i]  
-            link='check out this [link]('+url+')'
-            st.markdown(link,unsafe_allow_html=True)
+            link='[Read more......]('+url+')'
+            col3.markdown(link,unsafe_allow_html=True)
             st.divider()
     except:
         st.write("no News articles about the company in past 14 days")
