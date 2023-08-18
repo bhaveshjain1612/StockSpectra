ee8b5e99ac837946fac9eecfc52424a8fad46601
Bhavesh Jain <70514193+bhaveshjain1612@users.noreply.github.com>
2023-08-18 08:54:51 +0530

    Update collective_backend.py

--- backend_data/collective_backend.py
+++ backend_data/collective_backend.py
@@ -357,7 +357,7 @@ def collect_features(df):
 
 # Read the company list from the 'db_firmo.csv' file
 db_firmo = pd.read_csv("db_firmo.csv").head(10)
-symbol_list = db_firmo.Symbol#.head(10)
+symbol_list = db_firmo.head(10).Symbol
 kpi_df = pd.DataFrame()
 
 # Loop through the list of symbols, compile historical data and technical indicators, and generate features for each company
