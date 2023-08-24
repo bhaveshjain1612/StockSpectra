b75bde501f13cab563a9cda52cc17d185369ef79
Bhavesh Jain <70514193+bhaveshjain1612@users.noreply.github.com>
2023-08-24 10:05:26 +0530

    Update In Depth Stock Analysis.py

--- pages/In Depth Stock Analysis.py
+++ pages/In Depth Stock Analysis.py
@@ -317,10 +317,9 @@ def generate_news(name):
         st.write("no News articles about the company in past 14 days")
     else:
         st.header("Most Recent news articles (Last 14 Days)")
-
-    #st.dataframe(news)  
-    #for i in news.index:
-    #    st.subheader(news.title[i])
+        
+    for i in news.index:
+        st.subheader(news.title[i])
     #    col1, col2, col3 = st.columns([1,1,5])
     #    col1.write(news.source[i])
     #    col2.write(news.date[i])
