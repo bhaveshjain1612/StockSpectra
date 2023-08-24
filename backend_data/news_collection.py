23421772bf0c041bec259d2f408e9702a755d505
Bhavesh Jain <70514193+bhaveshjain1612@users.noreply.github.com>
2023-08-24 23:21:04 +0530

    Update news_collection.py

--- backend_data/news_collection.py
+++ backend_data/news_collection.py
@@ -80,7 +80,7 @@ def get_details(df):
 
     return df
 
-names = pd.read_csv("db_firmo.csv").Name.unique()
+names = pd.read_csv("db_firmo.csv").Symbol.unique()
 for n in tqdm(names):
     #print(n.replace(" ","_"))
     links = get_news(n)
